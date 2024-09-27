afafe8fc4d137e8e3ac764f44402d892d0b43dae
Troli <119256727+DevTroli@users.noreply.github.com>
2024-09-27 23:32:06 +0000

    🔐 Update envGen.py

--- contrib/envGen.py
+++ contrib/envGen.py
@@ -8,14 +8,13 @@ from django.utils.crypto import get_random_string
 
 chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
 
-CONFIG_STRING = (
-    """
+CONFIG_STRING = """
 DEBUG=True
 SECRET_KEY=%s
-ALLOWED_HOSTS=127.0.0.1
+ALLOWED_HOSTS=127.0.0.1, .localhost
 
-""".strip()
-    % get_random_string(50, chars)
+""".strip() % get_random_string(
+    50, chars
 )
 
 # Writing our configuration file to '.env'
