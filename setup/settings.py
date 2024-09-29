1cda2a60f7ff1b682abca5008be7fa8ee55e9cef
Pablo Troli <brawlixo123@gmail.com>
2024-09-28 22:41:01 -0300

    📦️ Add package django_extensions

--- setup/settings.py
+++ setup/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_extensions",
     "api.apps.ApiConfig",
     "order.apps.OrderConfig",
     "product.apps.ProductConfig",
